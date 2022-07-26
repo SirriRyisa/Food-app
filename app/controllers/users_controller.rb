4e1a361340e41059d27dbb053813e7bd5df42bc0
Munezero Christian <chamsf5@aol.com>
2022-07-26 15:42:59 +0200

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    def index
-        redirect_to new_user_session_path if current_user.nil?
-    end
+  def index
+    redirect_to new_user_session_path if current_user.nil?
+  end
 end
